3bf3975880928b6dd4162154545168714ff78cf1
822087-star <822087@manadulted.ac.uk>
2025-11-01 23:31:23 +0000

    this prog is testing

---
+++ Test_prog.py
@@ -0,0 +1,12 @@
+print("HelloWorld")
+name=input("what is your name :")
+birth_year=input("enter your date of birth:")
+print("Hello my dear :"   +  name)
+age=int(2025)-int(birth_year)
+print("your age is  : " , + age )
+
+
+
+
+
+
