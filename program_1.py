46b4233c0042cb521982a8fc0c39bab84940c513
822087-star <822087@manadulted.ac.uk>
2025-11-17 18:43:17 +0000

    task finish

--- program_1.py
+++ program_1.py
@@ -1,66 +1,91 @@
-
+#
 # List of available events (like a small library)
 events = ["Coding Workshop", "Roblox Tournament", "Science Fair", "Music Festival"]
-
+event_comer=[]
 print("=== Event Registration Program ===")
 
 # Main loop
 while True:
     # Get user info
-    name = input("Enter your name: ")
-    age = int(input("Enter your age: "))
+    name = input("Enter your name       : ")
+    age  = int(input("Enter your age    : "))
+if age <=10 :
+    print ("sorry you cannot participate this event because you are too young ")
+break
+#clear
+else:
+print ("you can can chose your event ")
+    
+for i. Event in event_comer:
 
-    # Display event list
+    participant = { 
+                    "name"  : name,
+                    "event" : selected_event,
+                    "age "  : "age",
+        }
+
+    event_comer.append(participant)
+
+    # Display event listparticipant
+    
     print("\nAvailable Events:")
-    for i, event in enumerate(events, start=1):
+    i=0
+    
+    for  event in enumerate(events, start=1):
         print(f"{i}. {event}")
 
     # Event choice
     choice = int(input("Choose an event number: "))
 
     # Validate choice
-    if 1 <= choice <= len(events):
+if 1 <= choice <= len(events):
         selected_event = events[choice - 1]
-        print(f"\n✔ {name}, age {age}, successfully registered for **{selected_event}**!\n")
-    else:
+else:
         print("\n✘ Invalid event number. Please try again.\n")
 
-    # Ask to continue
-    repeat = input("Do you want to register another person? (yes/no): ").lower()
-    if repeat != "yes":
-        print("\nThank you for using the Event Registration Program!")
-        break
+        print("================== who else comming for events list ==========================================\n")
+        print("======   NAME.        AGE                                              SELECTED EVENT.   •••• \n")
+        print("================================================================================================")
+        print("\n--- Registered Participants ---")
+
+if event_comer:
+    for event in event_comer():
+        print(event_comer())
+        print(f"Name: {participant['name']},Event: {participant['event_choice']},Age: {participant['age']}")
+else:
+    print("No participants registered.")
+    #return event_comer()
+
+if event_comer:
+        #for event in event_comer:
+            print(f"\n✔ {name}    , age :   {age}     , successfully registered for   **.    {selected_event}     **!\n")
+else:
+    print("  No participants reggistered. ")
+
+###Ask to continu
+
+repeat = input("Do you want to register another person? (yes/no): ").lower()
+if repeat != "yes":
+    values=event_comer.values()
+
+    print (event_comer.get("name"))
+
+    print("\n THANK YOU FO.R USING THE EVENT REGISTRATION PROGRAM!\n") 
+
+    #break
+#clear
 
 
 
-# List of available events (like a small library)
-events = ["Coding Workshop", "Roblox Tournament", "Science Fair", "Music Festival"]
+##########if participants_list:
 
-print("=== Event Registration Program ===")
+    #for participant in participants_list:
+        #print(f"Name test: {participant['name']}, Age: {participant['age']}, Event: {participant['event_choice']} \n")
+    #else:
+        #print("No participants registered.")
 
-# Main loop
-while True:
-    # Get user info
-    name = input("Enter your name: ")
-    age = int(input("Enter your age: "))
+    ####return participants_list
 
-    # Display event list
-    print("\nAvailable Events:")
-    for i, event in enumerate(events, start=1):
-        print(f"{i}. {event}")
 
-    # Event choice
-    choice = int(input("Choose an event number: "))
 
-    # Validate choice
-    if 1 <= choice <= len(events):
-        selected_event = events[choice - 1]
-        print(f"\n✔ {name}, age {age}, successfully registered for **{selected_event}**!\n")
-    else:
-        print("\n✘ Invalid event number. Please try again.\n")
 
-    # Ask to continue
-    repeat = input("Do you want to register another person? (yes/no): ").lower()
-    if repeat != "yes":
-        print("\nThank you for using the Event Registration Program!")
-        break
